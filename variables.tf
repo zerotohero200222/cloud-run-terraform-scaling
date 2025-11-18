b1cb77972769a9fa054e2e5e46b0d6839c3cf9be
zerotohero200222 <aebharath17@gmail.com>
2025-11-18 17:31:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,16 @@
 variable "project_id" {
-  description = "Project ID"
-  type        = string
+  type = string
 }
 
 variable "region" {
-  description = "Cloud Run region"
-  type        = string
-  default     = "us-central1"
+  type    = string
+  default = "us-central1"
 }
 
 variable "service_name" {
-  description = "Cloud Run service name"
-  type        = string
+  type = string
 }
 
 variable "max_instances" {
-  description = "Max instance count"
-  type        = number
+  type = number
 }
