ebf0bce367133ae3bccf19d93817ff327b133f19
zerotohero200222 <aebharath17@gmail.com>
2025-11-18 17:23:00 +0530

    Update and rename main-scaling.tf to main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Read existing Cloud Run service
+data "google_cloud_run_v2_service" "existing" {
+  name     = var.service_name
+  location = var.region
+}
+
+# Apply max instance scaling
+resource "google_cloud_run_v2_service" "scaling" {
+  name     = var.service_name
+  location = var.region
+
+  scaling {
+    max_instance_count = var.max_instances
+  }
+
+  template {
+    containers {
+      image = data.google_cloud_run_v2_service.existing.template[0].containers[0].image
+    }
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].containers[0].image,
+      template[0].containers[0].env,
+    ]
+  }
+}
