9219b60c7b4426d0df399e761b6cf68b993fc44b
zerotohero200222 <aebharath17@gmail.com>
2025-11-18 17:45:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,41 +14,28 @@ provider "google" {
   region  = var.region
 }
 
-# Read existing Cloud Run service
-data "google_cloud_run_v2_service" "existing" {
-  name     = var.service_name
-  location = var.region
-}
-
 ############################################
-# Cloud Run v2 – manage ONLY max instances
+# Cloud Run v2 – Manage ONLY max instances
 ############################################
-
-# NOTE: providers (google) and terraform { required_providers } 
-# stay in your existing provider.tf / versions.tf if you still have them.
-# This file only defines the Cloud Run service resource.
 
 resource "google_cloud_run_v2_service" "scaling" {
   project  = var.project_id
   name     = var.service_name
   location = var.region
 
-  # Allow Terraform to update the service config
-  deletion_protection = false
-
   template {
-    # We must specify at least one container image.
-    # Use the SAME image as your existing Cloud Run service.
-    # If your service currently uses a different image,
-    # update the image below or make it a variable.
     containers {
+      # Use the SAME image your Cloud Run service actually runs
+      # Replace if needed
       image = "us-docker.pkg.dev/cloudrun/container/hello"
     }
 
-    # 🔥 This is the only thing you care about:
+    # Only max instance scaling (as you requested)
     scaling {
       max_instance_count = var.max_instances
     }
   }
 }
+
+
 
